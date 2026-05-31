825f8b0750d2c7c99b4a5621fe0be2e61528a91e
Kommal Tariq <kommaltariq@gmail.com>
2026-05-31 17:21:57 +0400

    Update signal_mixer.m

--- src/signal_mixer.m
+++ src/signal_mixer.m
@@ -1,9 +1,13 @@
-function X_noisy = signal_mixer(S)
+function [X_noisy, S, A] = signal_mixer(voice, music)
 
-A = [1 0.5; 0.4 1];   % mixing matrix
+S = [voice; music];
+
+A = [1 0.5;
+     0.4 1];
+
 X = A * S;
 
-% Gaussian noise model
+% Gaussian noise (same as your original code)
 noise_power = 0.01 * var(X, 0, 2);
 noise = sqrt(noise_power) .* randn(size(X));
 
